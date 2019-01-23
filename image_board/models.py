574ff08e62e3f9b42241673eb8b08b5485308071
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-23 09:39:13 -0500

    updated import statements

--- image_board/models.py
+++ image_board/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
-from django.conf import settings
-from django.db import models
-from django.utils import timezone
-
 
 class Image(models.Model):
     image = models.URLField()
     title = models.CharField(max_length=200)
-    caption = models.TextField(null=True)
+    caption = models.TextField()
